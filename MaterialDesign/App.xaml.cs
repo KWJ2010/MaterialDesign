576d59f8897ba4c4d8d382024b23bec48b497538
KWJ2010 <kjohnson@epcon.com>
2016-10-25 16:44:16 -0500

    working on custom fonts w/o installing them locally

--- MaterialDesign/App.xaml.cs
+++ MaterialDesign/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace MaterialDesign
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
